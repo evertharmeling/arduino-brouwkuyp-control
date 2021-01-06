b562f70240a2db04644001e7792954214550fea4
Evert Harmeling <evertharmeling@gmail.com>
2021-01-06 13:54:12 +0100

    [code] Auto format (Tools > Auto format (cmd + t)

--- brew-platform/brew-platform.ino
+++ brew-platform/brew-platform.ino
@@ -1,16 +1,16 @@
 /**
- * Brouwkuyp Arduino Brew Software
- * @author Evert Harmeling <evertharmeling@gmail.com>
- */
- 
+   Brouwkuyp Arduino Brew Software
+   @author Evert Harmeling <evertharmeling@gmail.com>
+*/
+
 /**
- * @todo
- * - Connecting ArduinoClient to RabbitMQ within docker <hostIP:1883>?
- * - Warning: ISO C++ forbids converting a string constant to 'char*' [-Wwrite-strings]
- *    - char* p = "abc"; // valid in C, invalid in C++
- *    - char* p = (char*)"abc"; // OK
- *    - char const *p="abc"; // OK
- */
+   @todo
+   - Connecting ArduinoClient to RabbitMQ within docker <hostIP:1883>?
+   - Warning: ISO C++ forbids converting a string constant to 'char*' [-Wwrite-strings]
+      - char* p = "abc"; // valid in C, invalid in C++
+      - char* p = (char*)"abc"; // OK
+      - char const *p="abc"; // OK
+*/
 
 #include "env.h"
 #include <SPI.h>
@@ -93,7 +93,7 @@ void callback(char* topic, byte* payload, unsigned int length);
 
 // Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(PIN_SENSOR_TEMPERATURE);
-// Pass our oneWire reference to Dallas Temperature. 
+// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 EthernetClient ethClient;
 PubSubClient mqttClient(server, 1883, callback, ethClient);
@@ -113,289 +113,289 @@ char* pumpMode         = PUMP_MODE_AUTOMATIC;
 char* pumpState        = PUMP_STATE_OFF;
 
 /**
- *  Callback function to parse MQTT events
- */
-void callback(char* topic, byte* payload, unsigned int length) 
+    Callback function to parse MQTT events
+*/
+void callback(char* topic, byte* payload, unsigned int length)
 {
-    char value[length + 1];
-    snprintf(value, length + 1, "%s", payload);
-  
-    if (strcmp(topic, TOPIC_MLT_SET_TEMP) == 0) {
-        setTempMLT = atof(value);
-    } else if (strcmp(topic, TOPIC_PUMP_SET_STATE) == 0) {
-        if (strcmp(value, PUMP_STATE_ON) == 0) {
-            pumpMode = PUMP_MODE_MANUAL;
-            pumpState = PUMP_STATE_ON;
-            switchRelais(PIN_RELAIS_PUMP, true);
-        } else if (strcmp(value, PUMP_STATE_OFF) == 0) {
-            pumpMode = PUMP_MODE_MANUAL;
-            pumpState = PUMP_STATE_OFF;
-            switchRelais(PIN_RELAIS_PUMP, false);
-        } else {
-            pumpMode = PUMP_MODE_AUTOMATIC;
-        }
-    } else if (strcmp(topic, TOPIC_PUMP_SET_MODE) == 0) {
-        if (strcmp(value, PUMP_MODE_AUTOMATIC) == 0) {
-            pumpMode = PUMP_MODE_AUTOMATIC;
-        } else if (strcmp(value, PUMP_MODE_MANUAL) == 0) {
-            pumpMode = PUMP_MODE_MANUAL;
-        }
+  char value[length + 1];
+  snprintf(value, length + 1, "%s", payload);
+
+  if (strcmp(topic, TOPIC_MLT_SET_TEMP) == 0) {
+    setTempMLT = atof(value);
+  } else if (strcmp(topic, TOPIC_PUMP_SET_STATE) == 0) {
+    if (strcmp(value, PUMP_STATE_ON) == 0) {
+      pumpMode = PUMP_MODE_MANUAL;
+      pumpState = PUMP_STATE_ON;
+      switchRelais(PIN_RELAIS_PUMP, true);
+    } else if (strcmp(value, PUMP_STATE_OFF) == 0) {
+      pumpMode = PUMP_MODE_MANUAL;
+      pumpState = PUMP_STATE_OFF;
+      switchRelais(PIN_RELAIS_PUMP, false);
     } else {
-//        Serial.println("----------");
-//        Serial.print("Unknown / not listening to topic: ");
-//        Serial.println(topic);
-//        Serial.print("value: ");
-//        Serial.println(value);
-//        Serial.println("----------");
+      pumpMode = PUMP_MODE_AUTOMATIC;
     }
+  } else if (strcmp(topic, TOPIC_PUMP_SET_MODE) == 0) {
+    if (strcmp(value, PUMP_MODE_AUTOMATIC) == 0) {
+      pumpMode = PUMP_MODE_AUTOMATIC;
+    } else if (strcmp(value, PUMP_MODE_MANUAL) == 0) {
+      pumpMode = PUMP_MODE_MANUAL;
+    }
+  } else {
+    //        Serial.println("----------");
+    //        Serial.print("Unknown / not listening to topic: ");
+    //        Serial.println(topic);
+    //        Serial.print("value: ");
+    //        Serial.println(value);
+    //        Serial.println("----------");
+  }
 }
 
 void setup()
 {
-    Serial.begin(9600);
-    Ethernet.begin(mac, ip);
-    // give the Ethernet shield 500 milliseconds to initialize...
-    delay(500);
-    
-    sensors.begin();
-    Serial.print(sensors.getDeviceCount(), DEC);
-    Serial.println(" sensors found");
-  
-    // initialize Relais
-    pinMode(PIN_RELAIS_HLT_ONE,   OUTPUT);
-    pinMode(PIN_RELAIS_HLT_TWO,   OUTPUT);
-    pinMode(PIN_RELAIS_HLT_THREE, OUTPUT);
-    pinMode(PIN_RELAIS_MLT,       OUTPUT);
-    pinMode(PIN_RELAIS_PUMP,      OUTPUT);
+  Serial.begin(9600);
+  Ethernet.begin(mac, ip);
+  // give the Ethernet shield 500 milliseconds to initialize...
+  delay(500);
 
-    // set initial relais state
-    switchRelais(PIN_RELAIS_PUMP, false);    
-    switchRelais(PIN_RELAIS_MLT, false);
+  sensors.begin();
+  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.println(" sensors found");
+
+  // initialize Relais
+  pinMode(PIN_RELAIS_HLT_ONE,   OUTPUT);
+  pinMode(PIN_RELAIS_HLT_TWO,   OUTPUT);
+  pinMode(PIN_RELAIS_HLT_THREE, OUTPUT);
+  pinMode(PIN_RELAIS_MLT,       OUTPUT);
+  pinMode(PIN_RELAIS_PUMP,      OUTPUT);
+
+  // set initial relais state
+  switchRelais(PIN_RELAIS_PUMP, false);
+  switchRelais(PIN_RELAIS_MLT, false);
 }
 
 void loop()
 {
-    sensors.requestTemperatures();
+  sensors.requestTemperatures();
 
-    if (connectAndSubscribe()) {
-      
-        if (loopTime > LOOP_INTERVAL) {
-            handleRecipe();
-            publishData();
+  if (connectAndSubscribe()) {
 
-            loopTime = 0;
-        }
+    if (loopTime > LOOP_INTERVAL) {
+      handleRecipe();
+      publishData();
+
+      loopTime = 0;
     }
+  }
 
-    mqttClient.loop();
+  mqttClient.loop();
 }
 
 /**
- *  Handles the set variables, gotten from MQTT messages
- *
- *  Possible optimization for HLT temp, knowing the max MLT temp (last step), so HLT does not have to get warmer
- */
-void handleRecipe() 
+    Handles the set variables, gotten from MQTT messages
+
+    Possible optimization for HLT temp, knowing the max MLT temp (last step), so HLT does not have to get warmer
+*/
+void handleRecipe()
 {
-    // make sure we have a set temp for the MLT
-    if (setTempMLT != NULL) {
-        setTempHLT = setTempMLT + HLT_MLT_HEATUP_DIFF;
-        
-        if (setTempHLT > MAX_HLT_TEMPERATURE) {
-            setTempHLT = MAX_HLT_TEMPERATURE;
-        }
-        
-        heatUpHLT = handleHysterese(sensors.getTempC(sensorHLT), setTempHLT, heatUpHLT);
-        heatUpMLT = handleHysterese(sensors.getTempC(sensorMLT), setTempMLT, heatUpMLT);
-        
-        switchRelais(PIN_RELAIS_HLT_ONE,   heatUpHLT);
-        switchRelais(PIN_RELAIS_HLT_TWO,   heatUpHLT);
-        switchRelais(PIN_RELAIS_HLT_THREE, heatUpHLT);
-        
-        if (pumpMode == PUMP_MODE_AUTOMATIC) {
-            switchRelais(PIN_RELAIS_PUMP, heatUpMLT);
-            
-            if (heatUpMLT) {
-                pumpState = PUMP_STATE_ON;
-            } else {
-                pumpState = PUMP_STATE_OFF;
-            }
-            
-            // if pump is active and temp diff is more than MLT_HEATUP_DIFF, use extra heater under MLT            
-            if (heatUpMLT && (setTempMLT - sensors.getTempC(sensorMLT)) > MLT_HEATUP_DIFF) {
-                switchRelais(PIN_RELAIS_MLT, true);
-            } else {
-                switchRelais(PIN_RELAIS_MLT, false);
-            }
-        } else {
-            switchRelais(PIN_RELAIS_MLT, false);
-        }
+  // make sure we have a set temp for the MLT
+  if (setTempMLT != NULL) {
+    setTempHLT = setTempMLT + HLT_MLT_HEATUP_DIFF;
+
+    if (setTempHLT > MAX_HLT_TEMPERATURE) {
+      setTempHLT = MAX_HLT_TEMPERATURE;
     }
+
+    heatUpHLT = handleHysterese(sensors.getTempC(sensorHLT), setTempHLT, heatUpHLT);
+    heatUpMLT = handleHysterese(sensors.getTempC(sensorMLT), setTempMLT, heatUpMLT);
+
+    switchRelais(PIN_RELAIS_HLT_ONE,   heatUpHLT);
+    switchRelais(PIN_RELAIS_HLT_TWO,   heatUpHLT);
+    switchRelais(PIN_RELAIS_HLT_THREE, heatUpHLT);
+
+    if (pumpMode == PUMP_MODE_AUTOMATIC) {
+      switchRelais(PIN_RELAIS_PUMP, heatUpMLT);
+
+      if (heatUpMLT) {
+        pumpState = PUMP_STATE_ON;
+      } else {
+        pumpState = PUMP_STATE_OFF;
+      }
+
+      // if pump is active and temp diff is more than MLT_HEATUP_DIFF, use extra heater under MLT
+      if (heatUpMLT && (setTempMLT - sensors.getTempC(sensorMLT)) > MLT_HEATUP_DIFF) {
+        switchRelais(PIN_RELAIS_MLT, true);
+      } else {
+        switchRelais(PIN_RELAIS_MLT, false);
+      }
+    } else {
+      switchRelais(PIN_RELAIS_MLT, false);
+    }
+  }
 }
 
 /**
-  * Publishes all the data
-  */
-void publishData() 
-{    
-    publishFloat(TOPIC_MLT_CURR_TEMP, sensors.getTempC(sensorMLT));
-    publishFloat(TOPIC_HLT_CURR_TEMP, sensors.getTempC(sensorHLT));
-    publishFloat(TOPIC_BLT_CURR_TEMP, sensors.getTempC(sensorBLT));
-    
-    publishString(TOPIC_PUMP_CURR_MODE, pumpMode);
-    publishString(TOPIC_PUMP_CURR_STATE, pumpState);
+    Publishes all the data
+*/
+void publishData()
+{
+  publishFloat(TOPIC_MLT_CURR_TEMP, sensors.getTempC(sensorMLT));
+  publishFloat(TOPIC_HLT_CURR_TEMP, sensors.getTempC(sensorHLT));
+  publishFloat(TOPIC_BLT_CURR_TEMP, sensors.getTempC(sensorBLT));
 
-    // extra sensors for testing purposes
-    publishFloat(TOPIC_EXT_CURR_TEMP, sensors.getTempC(sensorEXT));
-    publishFloat(TOPIC_EXT2_CURR_TEMP, sensors.getTempC(sensorEXT2));
+  publishString(TOPIC_PUMP_CURR_MODE, pumpMode);
+  publishString(TOPIC_PUMP_CURR_STATE, pumpState);
+
+  // extra sensors for testing purposes
+  publishFloat(TOPIC_EXT_CURR_TEMP, sensors.getTempC(sensorEXT));
+  publishFloat(TOPIC_EXT2_CURR_TEMP, sensors.getTempC(sensorEXT2));
 }
 
 /******************
- * Help functions *
+   Help functions
  *****************/
 
 /**
- *  Connects to MQTT server and subscribes to topic
- */
-boolean connectAndSubscribe() 
+    Connects to MQTT server and subscribes to topic
+*/
+boolean connectAndSubscribe()
 {
-    if (!mqttClient.connected()) {
-        if (mqttClient.connect(MQTT_CLIENT, MQTT_USER, MQTT_PASS)) {
-            Serial.println("Connected!");
-            mqttClient.subscribe("brewery/#");
-            Serial.println("Subscribed!");
-            
-            return true;
-        } else {
-            Serial.println("Unable to connect!");
-        }
+  if (!mqttClient.connected()) {
+    if (mqttClient.connect(MQTT_CLIENT, MQTT_USER, MQTT_PASS)) {
+      Serial.println("Connected!");
+      mqttClient.subscribe("brewery/#");
+      Serial.println("Subscribed!");
+
+      return true;
+    } else {
+      Serial.println("Unable to connect!");
     }
+  }
 }
 
 /**
- *  Uniform way of handling the hysterese check, to prevent switching the relais on / off too fast
- *
- *  @param float currTemp   Current temperature value
- *  @param float setTemp    Desired temperature
- *  @param boolean heating  Boolean to hold current heating status
- */
+    Uniform way of handling the hysterese check, to prevent switching the relais on / off too fast
+
+    @param float currTemp   Current temperature value
+    @param float setTemp    Desired temperature
+    @param boolean heating  Boolean to hold current heating status
+*/
 boolean handleHysterese(float currTemp, float setTemp, boolean heating)
 {
-    float top = setTemp + HYSTERESE;
-    float bottom = setTemp - HYSTERESE;
+  float top = setTemp + HYSTERESE;
+  float bottom = setTemp - HYSTERESE;
 
-    if (currTemp >= bottom && currTemp <= top) {
-    } else if (currTemp < bottom) {
-        heating = true;
-    } else if (currTemp > top) {
-        heating = false;
-    }
+  if (currTemp >= bottom && currTemp <= top) {
+  } else if (currTemp < bottom) {
+    heating = true;
+  } else if (currTemp > top) {
+    heating = false;
+  }
 
-    return heating;
+  return heating;
 }
 
 /**
- *  Proxy method to publish a value to a topic in MQTT
- *
- *  @param char* topic
- *  @param char* value
- */
-void publishString(char* topic, char* value) 
+    Proxy method to publish a value to a topic in MQTT
+
+    @param char* topic
+    @param char* value
+*/
+void publishString(char* topic, char* value)
 {
-    if (mqttClient.connected()) {
-          char* jsonString = buildEvent(topic, value);
-          mqttClient.publish(topic, jsonString);
-          free(jsonString);
-    } else if (connectAndSubscribe()) {
-        publishString(topic, value);
-    }
+  if (mqttClient.connected()) {
+    char* jsonString = buildEvent(topic, value);
+    mqttClient.publish(topic, jsonString);
+    free(jsonString);
+  } else if (connectAndSubscribe()) {
+    publishString(topic, value);
+  }
 }
 
 /**
- *  Publishes a float to MQTT client
- *
- *  @param char* topic
- *  @param float value
- */
-void publishFloat(char* topic, float value) 
+    Publishes a float to MQTT client
+
+    @param char* topic
+    @param float value
+*/
+void publishFloat(char* topic, float value)
 {
-    if (value != NULL) {
-        char *charTemp = NULL;
-        
-        convertTemperature(value, &charTemp);
-        publishString(topic, charTemp);
-        free(charTemp);
-    }
+  if (value != NULL) {
+    char *charTemp = NULL;
+
+    convertTemperature(value, &charTemp);
+    publishString(topic, charTemp);
+    free(charTemp);
+  }
 }
 
 /**
- *  Create JSON event data object, this makes it possible to send more information at once.
- * 
- *  @param char* topic
- *  @param char* value
- */
+    Create JSON event data object, this makes it possible to send more information at once.
+
+    @param char* topic
+    @param char* value
+*/
 char* buildEvent(char* topic, char* value)
 {
-    char * buff = (char *)malloc(75);
-    const char *startTag = "{";
-    const char *topicTag = "\"topic\":\"";
-    const char *valueTag = "\",\"value\":\"";
-    const char *endTag = "\"}";
+  char * buff = (char *)malloc(75);
+  const char *startTag = "{";
+  const char *topicTag = "\"topic\":\"";
+  const char *valueTag = "\",\"value\":\"";
+  const char *endTag = "\"}";
 
-    strcpy(buff, startTag);
-    strcat(buff, topicTag);
-    strcat(buff, topic);
-    strcat(buff, valueTag);
-    strcat(buff, value);
-    strcat(buff, endTag);
+  strcpy(buff, startTag);
+  strcat(buff, topicTag);
+  strcat(buff, topic);
+  strcat(buff, valueTag);
+  strcat(buff, value);
+  strcat(buff, endTag);
 
-    return buff;
+  return buff;
 }
 
 /**
- *  Converts float to char
- *
- *  @param float temp
- *  @param char **charTemp
- */
-void convertTemperature(float temp, char **charTemp) 
+    Converts float to char
+
+    @param float temp
+    @param char **charTemp
+*/
+void convertTemperature(float temp, char **charTemp)
 {
-    *charTemp = (char *)malloc(sizeof(char) * (FLOAT_LENGTH + 1));
-    dtostrf(temp, FLOAT_LENGTH, PRECISION, *charTemp);
+  *charTemp = (char *)malloc(sizeof(char) * (FLOAT_LENGTH + 1));
+  dtostrf(temp, FLOAT_LENGTH, PRECISION, *charTemp);
 }
 
 /**
- *  Switch relais on (true) / off (false)
- *
- *  @param int relais     The pin of the relais
- *  @param boolean state  true / false
- */
-void switchRelais(int relais, boolean state) 
+    Switch relais on (true) / off (false)
+
+    @param int relais     The pin of the relais
+    @param boolean state  true / false
+*/
+void switchRelais(int relais, boolean state)
 {
-    if (state) {
-        //Serial.println("Set relais " + String(relais) + " ON");
-        //Serial.println("Set LED " + String(relais) + " OFF");
-        digitalWrite(relais, LOW);
-    } else {
-        //Serial.println("Set relais " + String(relais) + " OFF");
-        //Serial.println("Set LED " + String(relais) + " ON");
-        digitalWrite(relais, HIGH);
-    }
+  if (state) {
+    //Serial.println("Set relais " + String(relais) + " ON");
+    //Serial.println("Set LED " + String(relais) + " OFF");
+    digitalWrite(relais, LOW);
+  } else {
+    //Serial.println("Set relais " + String(relais) + " OFF");
+    //Serial.println("Set LED " + String(relais) + " ON");
+    digitalWrite(relais, HIGH);
+  }
 }
 
 /**
- *  Trims char array
- * 
- *  @param char* value
- */
-char* trim(char* value) 
+    Trims char array
+
+    @param char* value
+*/
+char* trim(char* value)
 {
-    char *s = value - 1;
-    char *e = value + strlen (value);
+  char *s = value - 1;
+  char *e = value + strlen (value);
 
-    while (++s < e && *s < 33);
-    while (--e > s && *e < 33);
-    *(++e) = (char) 0;
+  while (++s < e && *s < 33);
+  while (--e > s && *e < 33);
+  *(++e) = (char) 0;
 
-    return s;
+  return s;
 }
